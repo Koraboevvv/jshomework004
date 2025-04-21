afcd4f58301b4cb4048e2626bce1da9a34ac295f
Koraboevvv <bexruzkarabaev@gmail.com>
2025-04-21 23:59:32 +0500

    js4

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@ function darkMode(){
 }
 
 {
+    // M-1
     let kun = 9
     let javob =""
     switch (kun) {
@@ -34,9 +35,102 @@ function darkMode(){
     console.log(javob);
     
 }
+{
+    // M-2
+    let baho = 3
+    let javob =""
+    switch (baho) {
+        case 1:
+            javob ="Yomon"
+            break;
+        case 2:
+            javob ="Qoniqarsiz"
+            break;
+        case 3:
+            javob ="Qonqarli"
+            break;
+        case 4:
+            javob ="Yahshi"
+            break;
+        case 5:
+            javob ="A'lo"
+            break;
+        default:
+            javob ="Unaqa baho yok"
+            break;
+    }
+    console.log(javob);
+    
+}
 
+{
+    // M-3
+    let kun = 1
+    let javob =""
+    switch (kun) {
+        case 1:
+            case 2:
+                case 12:
+            javob ="Winter"
 
+            break;
+        case 3:
+            case 4:
+                case 5:
+            javob ="Spring"
 
+            break;
+        case 6:
+            case 7:
+                case 8:
+            javob ="Summer"
 
+            break;
+        case 9:
+            case 10:
+                case 11:
+            javob ="Auttumn"
+
+            break;
+        default:
+            javob ="Unaqa fasil yok"
+            break;
+    }
+    console.log(javob);
+    
+}
+{
+    // M-4
+    let kun = 1
+    let javob =""
+    switch (kun) {
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            javob ="31 kun"
+
+            break;
+        case 2:
+            javob ="28 kun"
+
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            javob ="30 kun"
+
+            break;
+        default:
+            javob ="Unaqa oylar yok"
+            break;
+    }
+    console.log(javob);
+    
+}
 
 
