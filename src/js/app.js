0b91313277aaea3eb8b5bcc6dc4e8018b9abd457
Koraboevvv <bexruzkarabaev@gmail.com>
2025-04-21 23:49:36 +0500

    js1

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,38 @@ function darkMode(){
     document.documentElement.classList.toggle("dark")
 }
 
-
+{
+    let kun = 9
+    let javob =""
+    switch (kun) {
+        case 0:
+            javob ="Yakshanba"
+            break;
+        case 1:
+            javob ="Dushanba"
+            break;
+        case 2:
+            javob ="Seyshanba"
+            break;
+        case 3:
+            javob ="Chorshanba"
+            break;
+        case 4:
+            javob ="Payshanba"
+            break;
+        case 5:
+            javob ="Juma"
+            break;
+        case 6:
+            javob ="Shanba"
+            break;
+        default:
+            javob ="Unaqa kun yok"
+            break;
+    }
+    console.log(javob);
+    
+}
 
 
 
